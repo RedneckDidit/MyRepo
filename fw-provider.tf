2326a71e69b5c6dc33a16a13b0ccd309600ff26e
RedneckDidit <nabilkandani@gmail.com>
2026-02-16 22:41:05 +0000

    Create fw-provider.tf

---
+++ fw-provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    panos = {
+      source  = "PaloAltoNetworks/panos"
+      version = "~> 1.11"
+    }
+  }
+}
+
+provider "panos" {
+  hostname = var.panos_hostname
+  api_key  = var.panos_api_key
+}
