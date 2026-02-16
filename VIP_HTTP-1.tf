148b757b32df4775e967e6a4b75bd98fae3cbe9b
RedneckDidit <nabilkandani@gmail.com>
2026-02-16 14:35:43 +0000

    Create VIP_HTTP-1.tf

---
+++ VIP_HTTP-1.tf
@@ -0,0 +1,11 @@
+
+
+
+frontend https_front
+    bind 10.10.10.10:80
+    default_backend http_back
+
+backend http_back
+    balance roundrobin
+    server web1 192.168.10.10:8080 check
+    server web2 192.168.10.10:8081 check
