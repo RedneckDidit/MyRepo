e3660bc34de689c4e8a1e66de5a1b088a4309cf6
RedneckDidit <nabilkandani@gmail.com>
2026-02-16 13:53:11 +0000

    Update rule-100.tf

--- rule-100.tf
+++ rule-100.tf
@@ -1,3 +1,12 @@
+provider "iptables" {
+  firewall_ip         = "192.168.0.1"
+  port                = 8080
+  allowed_cidr_blocks = ["10.0.0.0/24"]
+  https               = true
+  insecure            = true
+  vault_enable        = true
+}
+
 resource iptables_rules rules-front {
   name           = "rules-front"
   project        = iptables_project.theproject.name
