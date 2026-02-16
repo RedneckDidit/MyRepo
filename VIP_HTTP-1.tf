7085ce40c12a043b73eec29c95d9e9174350954f
RedneckDidit <nabilkandani@gmail.com>
2026-02-16 02:37:54 +0000

    Update VIP_HTTP-1.tf

--- VIP_HTTP-1.tf
+++ VIP_HTTP-1.tf
@@ -1,12 +1,14 @@
-
-
-
-frontend https_front
+resource "local_file" "vip1" {
+  content  = <<EOT
+frontend http_front-1
     bind 10.10.10.10:80
     default_backend http_back
 
 backend http_back
     balance roundrobin
-    server web1 192.168.10.10:8080 check
-    server web2 192.168.10.10:8081 check
+    server web1 192.168.1.10:8080 check
+EOT
+
+  filename = "${path.module}/haproxy_vip1.cfg"
+}
 
