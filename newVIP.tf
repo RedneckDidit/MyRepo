09be42f66fe07c144159ea6c3bc19a7b023933bf
RedneckDidit <nabilkandani@gmail.com>
2026-02-16 02:48:46 +0000

    Update newVIP.tf

--- newVIP.tf
+++ newVIP.tf
@@ -6,7 +6,32 @@ terraform {
     }
   }
 }
+resource "haproxy_backend" "backend_test" {
+  name                 = "backend_test"
+  mode                 = "http"
+  http_connection_mode = "http-keep-alive"
+  server_timeout       = 9
+  check_timeout        = 20
+  connect_timeout      = 20
+  queue_timeout        = 20
+  tarpit_timeout       = 20
+  tunnel_timeout       = 20
+  check_cache          = true
 
+  balance {
+    algorithm = "roundrobin"
+  }
+
+  httpchk_params {
+    uri     = "/health"
+    version = "HTTP/1.1"
+    method  = "GET"
+  }
+
+  forwardfor {
+    enabled = true
+  }
+}
 resource "haproxy_frontend" "front_test" {
   name                        = "front_test"
   backend                     = "backend_test"
@@ -37,32 +62,7 @@ resource "haproxy_frontend" "front_test" {
   depends_on = [haproxy_backend.backend_test]
 }
 
-resource "haproxy_backend" "backend_test" {
-  name                 = "backend_test"
-  mode                 = "http"
-  http_connection_mode = "http-keep-alive"
-  server_timeout       = 9
-  check_timeout        = 20
-  connect_timeout      = 20
-  queue_timeout        = 20
-  tarpit_timeout       = 20
-  tunnel_timeout       = 20
-  check_cache          = true
 
-  balance {
-    algorithm = "roundrobin"
-  }
-
-  httpchk_params {
-    uri     = "/health"
-    version = "HTTP/1.1"
-    method  = "GET"
-  }
-
-  forwardfor {
-    enabled = true
-  }
-}
 
 resource "haproxy_bind" "bind_test" {
   name        = "bind_test"
