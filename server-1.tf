4649ae52d59df035f1d7eadb91d8ff72508bea26
RedneckDidit <nabilkandani@gmail.com>
2026-02-17 00:37:26 +0000

    Update server-1.tf

--- server-1.tf
+++ server-1.tf
@@ -2,3 +2,9 @@ resource "panos_address_object" "server-1" {
   name  = "server-1"
   value = "10.10.10.10"
 }
+
+resource "panos_address_object" "server-2" {
+  name  = "server-2"
+  value = "10.10.10.20"
+}
+
