536402d55fb32455ed44d5266ecb73a7f3bce516
RedneckDidit <nabilkandani@gmail.com>
2026-02-17 12:12:51 +0000

    Update group1.tf

--- group1.tf
+++ group1.tf
@@ -2,10 +2,5 @@ resource "panos_address_group" "group-1" {
 
   name        = "group-1"
   description = "group-1"
-  static_address {
-      name = server-1
-  }
-  static_address {
-      name = server-2
-  }
+  
 }
