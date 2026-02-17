220a8b8ff4c464ab754760b0b4189f7c6043ce8e
RedneckDidit <nabilkandani@gmail.com>
2026-02-17 01:59:39 +0000

    Update fw-provider.tf

--- fw-provider.tf
+++ fw-provider.tf
@@ -11,15 +11,37 @@ provider "panos" {
   hostname = var.panos_hostname
   api_key  = var.panos_api_key
 }
+
+
+variable "group-object" {
+  description = "name of the group object"
+  {
+    type = list(object({
+    name        = string
+    description = string
+    type        = string
+  }))
+  }
+}
+
+resource "panos_address_group" "group-object" {
+
+  name        = var.group-object.name
+  description = var.group-object.description
+  static      = [for k in panos_address.address_objects : k.name]
+}
+
+
 variable "address_objects" {
   description = "List of address objects to create"
-  type = list(object({
+  {
     name        = string
     value       = string
     description = string
     type        = string
-  }))
+  }
 }
+
 
 resource "panos_address_object" "address_objects" {
   for_each = { for obj in var.address_objects : obj.name => obj }
