1aa679f42fbdfb898d66be6e439bdb14e7bcf0b4
RedneckDidit <nabilkandani@gmail.com>
2026-02-17 00:00:35 +0000

    Update fw-provider.tf

--- fw-provider.tf
+++ fw-provider.tf
@@ -10,4 +10,10 @@ terraform {
 provider "panos" {
   hostname = var.panos_hostname
   api_key  = var.panos_api_key
+timeouts {
+    create = "2m"
+    read   = "2m"
+    update = "2m"
+    delete = "2m"
+  }
 }
