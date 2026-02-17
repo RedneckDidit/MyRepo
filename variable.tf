3283d84ecb3df32ed4a37cb35db75cad31b2024d
RedneckDidit <nabilkandani@gmail.com>
2026-02-17 00:34:16 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,11 @@
-panos_hostname = "192.168.10.10"
+variable "panos_hostname" {
+  description = "Palo Alto firewall IP"
+  type        = string
+}
+
+variable "panos_api_key" {
+  description = "API key"
+  type        = string
+  sensitive   = true
+}
+
